00b2d7b4a1c8cd13f8e96a2833a9f273281fed68
Varun <eshanna@adobe.com>
2017-08-19 01:41:58 +0530

    first commit

---
+++ src/main/java/com/adobe/springboot/MyTestClass.java
@@ -0,0 +1,15 @@
+package com.adobe.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = "com.adobe.controller")
+public class MyTestClass {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyTestClass.class, args);
+	}
+
+}
