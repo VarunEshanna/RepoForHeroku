9ec8a0624e2299728565abfe5869ff87e66294b8
Varun <eshanna@adobe.com>
2017-08-19 02:26:34 +0530

    adding missing import

--- src/main/java/com/adobe/springboot/MyTestClass.java
+++ src/main/java/com/adobe/springboot/MyTestClass.java
@@ -3,6 +3,7 @@ package com.adobe.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.adobe.controller")
